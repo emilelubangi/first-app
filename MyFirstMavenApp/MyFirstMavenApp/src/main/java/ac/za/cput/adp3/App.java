29023fbadf30de789bd90d8b86ece1ecb3011593
emilelubangi <58505456+emilelubangi@users.noreply.github.com>
2020-02-23 15:09:33 +0200

    Joselle

--- MyFirstMavenApp/MyFirstMavenApp/src/main/java/ac/za/cput/adp3/App.java
+++ MyFirstMavenApp/MyFirstMavenApp/src/main/java/ac/za/cput/adp3/App.java
@@ -13,8 +13,8 @@ public class App
     public static void main( String[] args )
     { EmployeeGui app = new EmployeeGui();
         app.setVisible (true);
-        app.setSize(650, 500);
-        app.setLocation(200, 100);
+        app.setSize(650, 600);
+        app.setLocation(300, 100);
         app.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
 
     }
